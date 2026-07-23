85c708709812d5780ea682ccaa7781a65ecc319a
paulmurphyy <luappanda@gmail.com>
2026-07-23 10:06:38 -0500

    Refactor run_pipeline.py to include title extraction

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,17 +1,15 @@
 import subprocess, sys
-
+ 
 OCR_SCRIPT = "paddle_ocr_extract_pdf.py"
-MODEL_SCRIPT = "working_intel_model.py"
-
+TITLE_SCRIPT = "deepseek_extract_title.py"
+ 
 def main():
-    if len(sys.argv) < 4: 
-        print("[!] Usage: run_pipeline.py <in_pdf_path> <out_ocr_folder> <out_json_folder>")
+    if len(sys.argv) < 3:
+        print("[!] Usage: run_pipeline.py <in_pdf_folder> <out_ocr_folder>")
         sys.exit(1)
-
-    subprocess.run([sys.executable, OCR_SCRIPT, sys.argv[1], sys.argv[2]], check = True)
-    subprocess.run([sys.executable, MODEL_SCRIPT, sys.argv[2], sys.argv[3]], check = True)
-
-    print(f"JSON output for each page can be found in {sys.argv[3]}, numbered by page")
-
+    subprocess.run([sys.executable, OCR_SCRIPT, sys.argv[1], sys.argv[2]], check=True)
+    subprocess.run([sys.executable, TITLE_SCRIPT, sys.argv[2], sys.argv[1]], check=True)
+    print(f"Titled PDFs are in {sys.argv[1]}; see rename_log.csv there for details")
+ 
 if __name__ == "__main__":
     main()
